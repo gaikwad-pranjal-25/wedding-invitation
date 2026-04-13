42db87912f7968eceb5e1c6d80d5e0ff1836a982
The-red-Auror <pgaik2510@gmail.com>
2026-04-13 16:03:04 +0530

    photos updated and tap message added

--- script.js
+++ script.js
@@ -24,5 +24,5 @@ if (slides.length > 1) {
         slides[currentSlide].classList.remove("active");
         currentSlide = (currentSlide + 1) % slides.length;
         slides[currentSlide].classList.add("active");
-    }, 4000);
+    }, 2000);
 }
